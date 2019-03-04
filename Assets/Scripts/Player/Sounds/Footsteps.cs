228eb3c3f976901774aab01da2419dfc295cc32e
NarutoZabuza <zwz.dom@gmail.com>
2019-03-04 22:01:38 +0300

    some sound's and curve lvl

---
+++ Assets/Scripts/Player/Sounds/Footsteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool playerismoving = true;
+    public float currentSpeed = 6f;
+    
+    void CallFootsteps()
+    {
+        if (playerismoving) FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+    }
+	void Start () {
+        //currentSpeed = GameObject.FindGameObjectsWithTag("Player").GetComponent<
+        InvokeRepeating("CallFootsteps", 0, currentSpeed);
+	}
+	
+	
+}
