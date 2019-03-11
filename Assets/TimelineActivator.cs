ba1ce51206d29ebe20c31b03a6f93d3614da8699
ShMPMat <matveyshnytkin@gmail.com>
2019-03-11 18:53:08 +0300

    Shnytkins Curve location apdated with new Camera Movement

---
+++ Assets/TimelineActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineActivator : MonoBehaviour {
+    [SerializeField] PlayableDirector director;
+    [SerializeField] bool HowToDebug = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        director.Play();
+        HowToDebug = true;
+
+    }
+}
