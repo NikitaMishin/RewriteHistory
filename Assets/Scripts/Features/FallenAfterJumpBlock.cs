73bc223a860b5fb9064db4530bdf662be8f0da39
Alexey <alexzhukovwork@yandex.ru>
2019-03-22 22:42:50 +0300

    Added Fallen platform after jump

---
+++ Assets/Scripts/Features/FallenAfterJumpBlock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenAfterJumpBlock : MonoBehaviour {
+
+    [SerializeField] private int countForBreak = 3;
+    [SerializeField] private ManagerController _managerController;
+
+    private MeshRenderer _meshRenderer;
+    private Rigidbody _rigidbody;
+    private int _currentCount = 0;
+
+    private void Start()
+    {
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.tag.Equals("Player"))
+            return;
+
+        if (!_managerController.IsOnTheGround() && _managerController.jSpeed < 0)
+        {
+            _currentCount = _currentCount == countForBreak ? countForBreak : _currentCount + 1;
+            Debug.Log("Was Jumped");
+        }
+    }
+
+    public int GetCurrentCount()
+    {
+        return _currentCount;
+    }
+
+    public void SetCurrentCount(int value)
+    {
+        _currentCount = value;
+    }
+
+    public bool IsBroken()
+    {
+        return _currentCount == countForBreak;
+    }
+
+    public void SetMaterial(Material material)
+    {
+        _meshRenderer.material = material;
+    }
+
+    public void SetGravity(bool value)
+    {
+        _rigidbody.useGravity = value;
+    }
+
+    public bool GetGravity()
+    {
+        return _rigidbody.useGravity;
+    }
+}
---
+++ Assets/Scripts/Features/FallenAfterJumpController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenAfterJumpController : MonoBehaviour {
+
+    [SerializeField] private List<FallenAfterJumpBlock> fallenAfterJumpBlocks;
+    [SerializeField] private Material marker;
+    [SerializeField] private Material defaultMaterial;
+
+    private bool _wasBroken = false;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (_wasBroken)
+            return;
+
+        int countCurrentBroken = 0;
+
+        for (int i = 0; i < fallenAfterJumpBlocks.Count; i++)
+        {
+            if (fallenAfterJumpBlocks[i].IsBroken())
+            {
+                countCurrentBroken++;
+                fallenAfterJumpBlocks[i].SetMaterial(marker);
+            }
+            else
+            {
+                fallenAfterJumpBlocks[i].SetMaterial(defaultMaterial);
+            }
+        }
+
+        if (countCurrentBroken == fallenAfterJumpBlocks.Count)
+        {
+            SetGravity(true);
+            _wasBroken = true;
+        }
+	}
+
+    private void SetGravity(bool value)
+    {
+        for (int i = 0; i < fallenAfterJumpBlocks.Count; i++)
+        {
+            fallenAfterJumpBlocks[i].SetGravity(value);
+        }
+    }
+}
