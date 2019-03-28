1b7fe933321f7e4c379815b99e89ba8451c3e2c7
SasukeBack <37450825+SasukeBack@users.noreply.github.com>
2019-03-28 22:11:41 +0300

    Revert "Revert "Edited fallen platform""
    This reverts commit 9b8a2a5fa42e29d941cc61d0cedc629f83384af3.

--- Assets/Scripts/ReverseTime/TimePoints/RigidBodyTimePoint.cs
+++ Assets/Scripts/ReverseTime/TimePoints/RigidBodyTimePoint.cs
@@ -8,13 +8,15 @@ namespace ReverseTime
         public Quaternion Rotation;
         public Vector3 Velocity;
         public Vector3 AngularVelocity;
+        public bool useGravity;
 
-        public RigidBodyTimePoint(Vector3 pos, Quaternion rot, Vector3 velocity,Vector3 angularVelocity)
+        public RigidBodyTimePoint(Vector3 pos, Quaternion rot, Vector3 velocity,Vector3 angularVelocity, bool gravity)
         {
             Position = pos;
             Rotation = rot;
             Velocity = velocity;
             AngularVelocity = angularVelocity;
+            useGravity = gravity;
         }
     }
 }
--- Assets/Scripts/Triggers/FallTrigger.cs
+++ Assets/Scripts/Triggers/FallTrigger.cs
@@ -6,50 +6,24 @@ using UnityEngine;
 public class FallTrigger : MonoBehaviour {
 
     [SerializeField]
-    private float _fallSpeed = -5f;
-
-    [SerializeField]
     private float _secondsBeforeFall = 0.5f;
 
-    private GameObject _parent;
-    private float _defaultY;
+    private Rigidbody _rigidBody;
     private float _startTime;
-    private float _currentGravity;
-
-    private bool _isFallen;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-    void Awake()
-    {
-        _parent = gameObject.transform.parent.gameObject;
-        _defaultY = _parent.transform.position.y;
-    }
-
-	// Update is called once per frame
-	void Update () {
-
-        if (_isFallen)
-        {
-            _currentGravity += _fallSpeed * Time.deltaTime;
-            _parent.transform.position = _parent.transform.position + Vector3.up * Time.deltaTime * _currentGravity;
-        }
+        _rigidBody = gameObject.transform.parent.GetComponent<Rigidbody>();	
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-    //    if (other.gameObject.tag.Equals("Player"))
-        if (!_isFallen)
-            _startTime = Time.time;
-    
+        _startTime = Time.time;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (Time.time - _startTime > _secondsBeforeFall)
-            _isFallen = true;
+            _rigidBody.useGravity = true;
     }
 }
