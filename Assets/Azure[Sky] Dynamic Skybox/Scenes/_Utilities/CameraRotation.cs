b413ce15a4e44e6b9909ba11ea7febc395ce27d2
Alexey <alexzhukovwork@yandex.ru>
2019-03-30 19:06:44 +0300

    Added dynamic skybox

---
+++ Assets/Azure[Sky] Dynamic Skybox/Scenes/_Utilities/CameraRotation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	private Vector3 v3;
+	private Vector3 p3 = new Vector3(0f,0f,-6);
+	private float cameraZoon = 10.0f;
+	public float speed = 100;
+	public bool zoom = true;
+	
+	void Start()
+	{
+		v3= transform.localEulerAngles;
+	    //Application.targetFrameRate = 60;
+	}
+	
+	void LateUpdate()
+	{
+		if(Input.GetMouseButton(1) || Input.GetMouseButton(2))
+		{
+			
+			v3.x -= (Input.GetAxis("Mouse Y") * speed)*Time.deltaTime;
+			v3.y += (Input.GetAxis("Mouse X") * speed)*Time.deltaTime;      
+		}
+		v3=clamp(v3);
+		transform.localEulerAngles=v3;
+
+	}
+	
+	private Vector3 clamp(Vector3 v3)
+	{
+		if (v3.x>360)v3.x-=360;
+		if (v3.x<-360)v3.x+=360;
+		if (v3.y>360)v3.y-=360;
+		if (v3.y<-360)v3.y+=360;
+		return v3;
+	}
+}
