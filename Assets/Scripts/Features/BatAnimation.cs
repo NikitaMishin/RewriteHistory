128b70c7b44084671d5830b7fc19d39402544618
Alexey <alexzhukovwork@yandex.ru>
2019-03-31 20:43:54 +0300

    Added simple bats prefab

---
+++ Assets/Scripts/Features/BatAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatAnimation : MonoBehaviour {
+
+    [SerializeField] private BezierCurveMovementWithRewind bezierCurveMovementWithRewind;
+
+    private Animator[] _betAnimators;
+
+    private bool _wasEnabled = false;
+
+    private void Start()
+    {
+        _betAnimators = gameObject.GetComponentsInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (bezierCurveMovementWithRewind.wasStepped && !_wasEnabled)
+        {
+            _wasEnabled = true;
+            for (int i = 0; i < _betAnimators.Length; i++)
+            {
+                _betAnimators[i].enabled = true;
+            }
+        } else if (!bezierCurveMovementWithRewind.wasStepped && _wasEnabled)
+        {
+            _wasEnabled = false;
+            for (int i = 0; i < _betAnimators.Length; i++)
+            {
+                _betAnimators[i].enabled = false;
+            }
+        }
+	}
+}
