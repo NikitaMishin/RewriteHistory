0dd2605d1e43329adae2e42fc4d620e2dd8f13df
aakomarov <lfc2011sadsa@gmail.com>
2019-05-05 22:30:40 +0300

    LvlArt

--- Assets/Azure[Sky] Dynamic Skybox/Scenes/_Utilities/AzureUIController.cs
+++ Assets/Azure[Sky] Dynamic Skybox/Scenes/_Utilities/AzureUIController.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.AzureSky;
 
 public class AzureUIController : MonoBehaviour
 {
 	public AzureSkyController skyController;
-	public Slider slider;
-	public Image transitionBar;
-	private Vector3 m_scale;
+	//public Slider slider;
+	//public ima transitionBar;
+	//private Vector3 m_scale;
 
-	// Update is called once per frame
-	void Update ()
-	{
-		skyController.timeOfDay.hour = slider.value;
-		if (transitionBar)
-		{
-			m_scale = new Vector3 (skyController.weatherTransitionTime, 1.0f, 1.0f);
-			transitionBar.rectTransform.localScale = m_scale;
-		}
-	}
+	//// Update is called once per frame
+	//void Update ()
+	//{
+	//	skyController.timeOfDay.hour = slider.value;
+	//	if (transitionBar)
+	//	{
+	//		m_scale = new Vector3 (skyController.weatherTransitionTime, 1.0f, 1.0f);
+	//		transitionBar.rectTransform.localScale = m_scale;
+	//	}
+	//}
 }
