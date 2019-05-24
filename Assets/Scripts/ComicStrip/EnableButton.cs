f7c17c4be351ac8acd6e2a1d0df24a77fa281b6a
Alexey <alexzhukovwork@yandex.ru>
2019-05-25 02:06:32 +0300

    Good

---
+++ Assets/Scripts/ComicStrip/EnableButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableButton : MonoBehaviour
+{
+
+	[SerializeField] private Animator animator;
+	[SerializeField] private Button button;
+
+	private void Start()
+	{
+		button = FindObjectOfType<Button>();
+	}
+
+	public void EnableButtonNext()
+	{
+		button.interactable = true;
+    	animator.SetTrigger("Enable");
+    	animator.ResetTrigger("Disable");
+	}
+}
